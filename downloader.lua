820460324e7990fcd0d5a2cbbe4978932037cd82
razorwarecc <108592787+razorwarecc@users.noreply.github.com>
2024-04-29 00:24:39 +0000

    Create downloader.lua

---
+++ downloader.lua
@@ -0,0 +1,11 @@
+if not isfolder("fentanyl.club/hitsounds") then makefolder("fentanyl.club/hitsounds") end
+if not isfile("fentanyl.club/hitsounds/banana.mp3") then writefile("fentanyl.club/hitsounds/banana.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/banana.mp3")) end
+if not isfile("fentanyl.club/hitsounds/generic.mp3") then writefile("fentanyl.club/hitsounds/generic.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/generic.mp3")) end
+if not isfile("fentanyl.club/hitsounds/lol.mp3") then writefile("fentanyl.club/hitsounds/lol.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/lol.mp3")) end
+if not isfile("fentanyl.club/hitsounds/minecraft.mp3") then writefile("fentanyl.club/hitsounds/minecraft.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/minecraft.mp3")) end
+if not isfile("fentanyl.club/hitsounds/neverlose.mp3") then writefile("fentanyl.club/hitsounds/neverlose.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/neverlose.mp3")) end
+if not isfile("fentanyl.club/hitsounds/pokimane.mp3") then writefile("fentanyl.club/hitsounds/pokimane.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/pokimane.mp3")) end
+if not isfile("fentanyl.club/hitsounds/rust.mp3") then writefile("fentanyl.club/hitsounds/rust.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/rust.mp3")) end
+if not isfile("fentanyl.club/hitsounds/skeet.mp3") then writefile("fentanyl.club/hitsounds/skeet.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/skeet.mp3")) end
+if not isfile("fentanyl.club/hitsounds/tf2.mp3") then writefile("fentanyl.club/hitsounds/tf2.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/tf2.mp3")) end
+if not isfile("fentanyl.club/hitsounds/uwu.mp3") then writefile("fentanyl.club/hitsounds/uwu.mp3", game:HttpGet("https://raw.githubusercontent.com/razorwarecc/fentanylhitsounds/main/uwu.mp3")) end
